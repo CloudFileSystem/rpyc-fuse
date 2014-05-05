d30ce2eb517db62d89113d7d7d453ad7019eb40a
Naoya Sawada <naoya@localhost.localdomain>
2014-05-05 15:05:44 +0900

    modify deploy script

--- dist/ubuntu.sh
+++ dist/ubuntu.sh
@@ -23,17 +23,5 @@ easy_install pip
 #
 sudo pip install virtualenv
 
-#
-# activate virtualenv
-#
-virtualenv .
-source ./bin/activate
-
-#
-# install virtualenv
-#
-pip install fuse-python
-pip install fusepy
-
 # vim: set nu ts=2 autoindent : #
 
